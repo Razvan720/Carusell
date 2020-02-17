66741a0935a460d353acc698a7303311a6e3d787
Razvan720 <velsi12razvan@gmail.com>
2020-02-17 21:41:57 +0100

    Hago la funcion ver archivos

---
+++ src/java/modelo/Utilidades.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DAW209
+ */
+public class Utilidades {
+
+	public static ArrayList<String> getArchivos(String sDirectorio) {
+		ArrayList<String> archivos = new ArrayList<String>();
+		File dir = new File(sDirectorio);
+		String[] ficheros = dir.list();
+
+		if (ficheros != null) {
+			for (int x = 0; x < ficheros.length; x++) {
+				if (ficheros[x].endsWith(".jpg") || ficheros[x].endsWith(".gif") || ficheros[x].endsWith(".png")) {
+					
+				}
+			}
+		}
+
+		return archivos;
+	}
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : 17-feb-2020, 21:09:14
+    Author     : DAW209
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+		
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <a href="carousel.jsp">Ir a la aplicacion</a>
+    </body>
+</html>
